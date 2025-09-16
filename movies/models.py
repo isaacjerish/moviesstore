ca42dff9ead58ea3afdd640545eb6575d3adc965
Isaac Jerish <152412476+isaacjerish@users.noreply.github.com>
2025-09-15 21:02:26 -0400

    added release year

--- movies/models.py
+++ movies/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Movie(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
-    image = models.ImageField(upload_to='movie_images/')
+    image = models.ImageField(upload_to="movie_images/")
+    release_year = models.IntegerField(default=2000)
 
     def __str__(self):
-        return str(self.id) + ' - ' + self.name
+        return str(self.id) + " - " + self.name
+
 
 class Review(models.Model):
     id = models.AutoField(primary_key=True)
@@ -19,4 +22,4 @@ class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return str(self.id) + ' - ' + self.movie.name
+        return str(self.id) + " - " + self.movie.name
